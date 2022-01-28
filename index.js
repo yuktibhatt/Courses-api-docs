b073b15d891b59c8f2ad4a85a04f727edaba4dc5
yuktibhatt <bhattyukti5@gmail.com>
2022-01-28 21:28:17 +0545

    getting started

---
+++ index.js
@@ -0,0 +1,16 @@
+const express= require('express');
+
+const app = express()
+
+//swagger
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/", (req, res) => {
+    res.send("Hello there.. ");
+})
+
+app.listen(4000, () => console.log(`Server is running at port 4000..`));
