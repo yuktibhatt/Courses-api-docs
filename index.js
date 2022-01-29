61875809e2c2ae844c0e89bd6434f30b7b2b8a26
yuktibhatt <bhattyukti5@gmail.com>
2022-01-29 17:40:01 +0545

    handling file upload

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const app = express()
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
-//const fileUpload = require("express-fileupload");
+const fileUpload = require("express-fileupload");
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
-//app.use(fileUpload());
+app.use(fileUpload());
 
 let courses =[
     {
@@ -60,6 +60,16 @@ app.get("/api/v1/coursequery", (req,res) => {
     let location = req.query.location
     let device = req.query.device
     res.send({ location, device });
+});
+
+app.post("/api/v1/courseupload", (req,res) => {
+    const file = req.files.file;
+    let path = __dirname + "/images/" + Date.now() + ".jpg";
+
+    file.mv(path, (err) => {
+        res.send(true);
+    });
+   
 });
 
 
